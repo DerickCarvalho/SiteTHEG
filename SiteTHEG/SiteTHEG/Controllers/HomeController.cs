b20993af86b5f62b55c5e81dc55dfb1fbc9ccb49
emanoelheron <emanoelheron@gmail.com>
2023-09-30 03:26:11 -0300

    backupCommit_30/09/23

--- SiteTHEG/SiteTHEG/Controllers/HomeController.cs
+++ SiteTHEG/SiteTHEG/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SiteTHEG.Controllers
             return View();
         }
 
+        public IActionResult TelaRegistroPc()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
