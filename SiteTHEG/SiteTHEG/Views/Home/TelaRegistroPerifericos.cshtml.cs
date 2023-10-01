aa981ca1ec5d45159a6c82d86613f11843fda82f
emanoelheron <emanoelheron@gmail.com>
2023-10-01 02:53:24 -0300

    01/10/2023

---
+++ SiteTHEG/SiteTHEG/Views/Home/TelaRegistroPerifericos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SiteTHEG.Views.Home
+{
+    public class TelaRistroPerifericosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
